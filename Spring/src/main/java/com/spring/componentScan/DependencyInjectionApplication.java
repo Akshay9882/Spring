a055f77d53b31023ee2aee6fee1f3b554cead3a4
telia <akshayteli@yahoo.com>
2020-03-28 23:00:01 +0530

    Using ComponentScan

--- Spring/src/main/java/com/spring/componentScan/DependencyInjectionApplication.java
+++ Spring/src/main/java/com/spring/componentScan/DependencyInjectionApplication.java
@@ -1,11 +1,15 @@
-package com.spring;
+package com.spring.componentScan;
 
 import com.spring.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+//ComponentScan-will scan for all components in given path
 
 @SpringBootApplication
+@ComponentScan("com.spring")
 public class DependencyInjectionApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
