f3f2716798fb716b9977bb37d14d867b33854e23
telia <akshayteli@yahoo.com>
2020-03-29 12:09:43 +0530

    Removing unused imports

--- Spring/src/main/java/com/spring/services/HelloWorldServiceEnglishImpl.java
+++ Spring/src/main/java/com/spring/services/HelloWorldServiceEnglishImpl.java
@@ -1,8 +1,5 @@
 package com.spring.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
 public class HelloWorldServiceEnglishImpl implements HelloWorldService{
     @Override
     public String getGreeting() {
